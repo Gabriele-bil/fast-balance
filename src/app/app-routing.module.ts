bb20b53a056ca409dd681e861cdd432b97f5377b
Gabriele-Bil <gabriele.bilello1@gmail.com>
2021-07-04 12:07:03 +0200

    implemented login guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginGuard } from "@shared/guards/login.guard";
 
 const authModule = () => import('./features/auth/auth.module').then(a => a.AuthModule);
 const dashboardModule = () => import('./features/dashboard/dashboard.module').then(d => d.DashboardModule);
 
 const routes: Routes = [
   { path: '', loadChildren: authModule },
-  { path: 'dashboard', loadChildren: dashboardModule },
+  { path: 'dashboard', loadChildren: dashboardModule, canActivate: [LoginGuard] },
   { path: '**', redirectTo: '' }
 ];
 
