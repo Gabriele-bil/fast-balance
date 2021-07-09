4c62389c1d98ea31011f69d035cd386bd766427b
Gabriele-Bil <gabriele.bilello1@gmail.com>
2021-07-09 21:40:05 +0200

    implemented module,routing and container for settings page

--- src/app/features/dashboard/dashboard-routing.module.ts
+++ src/app/features/dashboard/dashboard-routing.module.ts
@@ -3,14 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardContainerComponent } from "./components/dashboard-container.component";
 
 const welcomeModule = () => import('./welcome/welcome.module').then(w => w.WelcomeModule);
+const settingsModule = () => import('./settings/settings.module').then(s => s.SettingsModule);
 
 const routes: Routes = [
-  { path: '', component: DashboardContainerComponent,
+  {
+    path: '', component: DashboardContainerComponent,
     children: [
-      { path: '', loadChildren: welcomeModule }
-    ]
-  }
-];
+      { path: '', redirectTo: 'welcome' },
+      { path: 'welcome', loadChildren: welcomeModule },
+      { path: 'settings', loadChildren: settingsModule }
+    ],
+  },
+]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
---
+++ src/app/features/dashboard/settings/components/settings-container.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-settings-container',
+  template: `
+    <p>
+      settings-container works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class SettingsContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
