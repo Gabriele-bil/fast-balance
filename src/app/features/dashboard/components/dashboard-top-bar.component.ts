79742e765d000351b61ee31823c00e34e55648a4
Gabriele Bilello <gabriele.bilello1@gmail.com>
2021-07-09 22:46:53 +0200

    implemented ui

--- src/app/features/dashboard/components/dashboard-top-bar.component.ts
+++ src/app/features/dashboard/components/dashboard-top-bar.component.ts
@@ -3,14 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-dashboard-top-bar',
   template: `
-    <div id="container" class="w-100 d-flex justify-content-between align-items-center px-3">
-      <h1 class="mb-0">Fast Balance</h1>
-      <div class="card-user d-flex align-items-center p-2">
-        <div class="image-container p-1 me-1">
-          <img src="/assets/images/user-icon.png" alt="User image icon">
-        </div>
-        <h2 class="mb-0 mx-2">Nome utente</h2>
-        <i class="bi bi-arrow-down-short"></i>
+    <div
+      id="container"
+      class="w-100 d-flex justify-content-between align-items-center px-3"
+    >
+      <h1 class="mb-0 cursor-pointer" routerLink="/dashboard">Fast Balance</h1>
+
+      <div class="dropdown">
+        <button
+          class="btn dropdown-toggle card-user d-flex align-items-center justify-content-center"
+          type="button"
+          id="dropdownMenuButton1"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          <div class="image-container p-1 me-1">
+            <img src="/assets/images/user-icon.png" alt="User image icon" />
+          </div>
+          <h2 class="mb-0 mx-2">Nome utente</h2>
+          <i class="bi bi-caret-down-fill mt-1"></i>
+        </button>
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+          <li><a class="dropdown-item" routerLink="settings">Action</a></li>
+          <li><a class="dropdown-item" href="#">Another action</a></li>
+          <li><a class="dropdown-item" href="#">Something else here</a></li>
+        </ul>
       </div>
     </div>
   `,
@@ -22,10 +39,10 @@ import { Component } from '@angular/core';
         background-color: #fafafa;
 
         .card-user {
-          border: 2px solid #111112;
           cursor: pointer;
 
-          &:hover {
+          &:hover,
+          &:focus {
             transition: ease-in-out 0.3s;
             box-shadow: 2px 2px 7px #888888;
           }
@@ -40,11 +57,23 @@ import { Component } from '@angular/core';
           }
 
           i {
-            font-size: 1.5rem;
+            font-size: 1rem;
+          }
+
+          &.dropdown-toggle {
+            width: 240px;
+
+            &::after {
+              display: none;
+            }
           }
         }
+
+        .dropdown-menu {
+          width: 240px;
+        }
       }
-    `]
+    `,
+  ],
 })
-export class DashboardTopBarComponent {
-}
+export class DashboardTopBarComponent {}
